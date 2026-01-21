972d3d41f81bc3f5eb0d7146486d78099829c3bc
Amgain <surajamgain9@gmail.com>
2026-01-21 20:00:25 +0545

    Frontend

---
+++ TPay_Frontend/milan-payment-app/src/components/UI.jsx
@@ -0,0 +1,15 @@
+// export const SectionHeader = ({ title }) => (
+//   <h3 className="text-gray-800 font-bold text-lg mb-3 px-1 flex justify-between">
+//     {title}
+//     <span className="text-primary text-sm">See All</span>
+//   </h3>
+// );
+
+// export const ActionButton = ({ icon: Icon, label, colorClass }) => (
+//   <button className="flex flex-col items-center p-4 rounded-2xl bg-white shadow-sm">
+//     <div className={`p-3 rounded-full ${colorClass}`}>
+//       {/* <Icon /> */}
+//     </div>
+//     <span className="text-xs font-semibold mt-2">{label}</span>
+//   </button>
+// );
